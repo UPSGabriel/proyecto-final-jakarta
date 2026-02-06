3aa8d28e94941f71e19f4bd660697cd60e69de09
Patricio Pacheco Sarmiento <danielpacheco842005@gmail.com>
2026-02-06 02:08:58 -0500

    Arreglo CORS SPRINGBOOT

--- modulo-spring/reportes-api/reportes-api/src/main/java/ec/edu/ups/reportes/api/CorsConfig.java
+++ modulo-spring/reportes-api/reportes-api/src/main/java/ec/edu/ups/reportes/api/CorsConfig.java
@@ -13,8 +13,9 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") 
-                        .allowedOrigins("*") 
+                registry.addMapping("/**")
+                   
+                        .allowedOriginPatterns("*") 
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
