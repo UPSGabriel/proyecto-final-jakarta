00b0bc9a27d8466b56b6b2dccbf15d5007fe3e49
Patricio Pacheco Sarmiento <danielpacheco842005@gmail.com>
2026-02-05 23:20:33 -0500

    Configuracion para deploy de Springboot

--- modulo-python/database.py
+++ modulo-python/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:odoo@localhost:5432/proyecto_final_db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:hBInKYStGCFGDNVfYkdKJojqwSEEwbJX@mainline.proxy.rlwy.net:57516/railway"
 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
